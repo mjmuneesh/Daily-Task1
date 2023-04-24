e75d725f136a96bbf7f33ccb5b9acd1944b6322b
mj_muneesh <muneeshsharma094@gmail.com>
2023-04-24 16:11:34 +0530

    let , const and var keywords

--- 02/demos/after/js-demo/home.js
+++ 02/demos/after/js-demo/home.js
@@ -6,9 +6,22 @@
 
 // showmessage(price);  /* we can use many variables and it will change accordingly */
 
-const price= 55;
+// const price= 55;
 
 // price = 44.99; /* this will not happen as in const we cannot change the value*/
 
-showmessage(price);
+// showmessage(price);
 
+// var price = 25;
+
+// showmessage (price); /* this is showing the price as 25 as we are declaring it but if we dont declare it , it will still not show the error */
+
+// showmessage (person)
+// console.log(person);
+// var person = "muneesh";
+
+// even when we are consloing it it will show us the undefined , that way it is best to use the let keyword instead
+
+let price = 25;
+showmessage(price);
+console.log(price);
