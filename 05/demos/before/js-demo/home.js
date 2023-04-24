48af943b6259e2baee51ef6bd3987c3b14471ca3
mj_muneesh <muneeshsharma094@gmail.com>
2023-04-24 17:08:03 +0530

    operators

--- 05/demos/before/js-demo/home.js
+++ 05/demos/before/js-demo/home.js
@@ -1,7 +1,96 @@
 
+// let price = 25.99;
+// price = price + 1;
+// showMessage(price); 
+
+// // this is the addition operator 
+
+// let price = 25.99;
+// price = price - 1;
+// showMessage(price); 
+
+// this is the subtraction operator
+
+// let price = 12;
+// price = price * 4;
+// showMessage(price); 
+
+// this is the star  operator
+
+// let price = 12;
+// price = price / 2;
+// showMessage(price); 
+
+// this is the division operator
+
+// let price = 12;
+// price = price % 5;
+// showMessage(price); 
+
+// this is the modulus operator( it shows the remainder )
 
 
-let price = 25;
 
-showMessage(price);
-console.log(price);
+
+// let price = 25.99;
+// price += 5;
+// showMessage(price); 
+// this will store the value of price = price +5 in price 
+
+
+
+// let price = 25.99;
+// price -= 5;
+// showMessage(price); 
+// this will store the value of price = price -5 in price 
+
+
+// let price = 25.99;
+// price *= 5;
+// showMessage(price); 
+// this will store the value of price = price *5 in price 
+
+
+// let price = 25.99;
+// price /= 5;
+// showMessage(price); 
+// this will store the value of price = price /5 in price 
+
+
+// let price = 25.99;
+// price %= 5;
+// showMessage(price); 
+// this will store the value of price = price %5 in price 
+
+
+// let price = 25;
+//     taxrate = 2;
+// showMessage(price * taxrate); 
+
+// this way we can do operations with multiple variables 
+
+
+// let price = 25.99;
+// showMessage(++price); 
+
+// this is the increment operator 
+
+// let price = 25.99;
+// showMessage(price++); 
+// console.log(price);
+
+//  this is the commmon bug in the javascript where after the variable if we put the increment operator it will wait until the line of code is executed  and then it increment the variable
+
+// let price = 25.99;
+// showMessage(--price); 
+// console.log(price);
+
+// this is the decrement operator
+
+
+
+
+
+
+
+
