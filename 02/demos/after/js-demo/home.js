9bd624f33e96133801f5fd78271aeb62d0b09799
mj_muneesh <muneeshsharma094@gmail.com>
2023-04-24 15:49:27 +0530

    learned what is const

--- 02/demos/after/js-demo/home.js
+++ 02/demos/after/js-demo/home.js
@@ -1,5 +1,14 @@
-let discount = false;
-name = "Muneesh";
-price = 49.99;
+// let discount = false;
+// name = "Muneesh";
+// price = 49.99;
 
-showmessage(name);  /* we can use many variables and it will change accordingly */
+// price = 99.99; /* we can change the price as many times we want */
+
+// showmessage(price);  /* we can use many variables and it will change accordingly */
+
+const price= 55;
+
+// price = 44.99; /* this will not happen as in const we cannot change the value*/
+
+showmessage(price);
+
