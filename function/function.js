e60402081c77f8c8f8273c6b3ab612767e980a19
mj_muneesh <muneeshsharma094@gmail.com>
2023-04-28 17:24:07 +0530

    Function

---
+++ function/function.js
@@ -0,0 +1,34 @@
+// functions
+
+
+
+// function multioftwo(a,b){
+//   const multi  = a*b;
+//   console.log(multi);
+// }
+
+// let a=5;
+// b=77;
+// multioftwo(a,b);
+
+
+
+
+
+// function squareoftwoNum(){
+//  return (a*a);  
+// }
+
+// let a=9;
+// const Num = squareoftwoNum();
+// console.log(Num);
+
+
+
+// function expression 
+// in function expression hoisting is not done which means that we have to declare the function first 
+
+// const addition = (num)=>{
+//     return num + num;
+// }
+//  console.log(addition(5));
