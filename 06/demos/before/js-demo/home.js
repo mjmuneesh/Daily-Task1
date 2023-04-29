d2427c33b0cba049763225d8f8127aaaf29b7bfa
mj_muneesh <muneeshsharma094@gmail.com>
2023-04-29 17:56:27 +0530

    IF ELSE AND LOOPS

--- 06/demos/before/js-demo/home.js
+++ 06/demos/before/js-demo/home.js
@@ -1,4 +1,94 @@
-let price = 20;
-if(price > 15){
-    showMessage('discounted')
-}
+// IF ELSE STATEMENTS 
+
+// let price = 6;
+// if(price > 15){
+//     showMessage('discounted');
+// }
+// else if (price <10) {
+//     showMessage('less than 10');
+// }
+
+
+
+
+
+// COMPARISON (==) AND (===)
+
+
+// if (1=="1"){
+//     showMessage("true");
+
+// }
+// else {
+//     showMessage("false");
+// }
+
+
+
+// if (1==="1"){    // this will check the data type 
+//     showMessage("true");
+
+// }
+// else {
+//     showMessage("false");
+// }
+
+
+//TERNARY OPERATOR 
+
+// let item =(price >10) ? 'expensive' : 'cheap';
+
+// showMessage(item);
+
+
+
+// another example  
+
+//  let price = 5;
+
+//  let message =  (price >10) ? 'yes' : 'no';
+//  showMessage(message);
+
+
+
+
+//BLOCK SCOPE USING LET 
+  
+// if (true){
+//     let value = 'yes';
+//     showMessage(value)
+// }
+
+
+// console.log(value); //we cannot access the value outside the code block  but in var case the variable value leaked from the code block , so use let and const which is the best practice 
+
+
+//FOR LOOP 
+
+// for (let i=0; i<5; i++){
+//     console.log(i);
+// }
+
+
+
+//WHILE LOOP 
+
+// let count =1;
+// while (count < 5){
+//     console.log(count);
+//     count++;
+// }
+
+
+
+//DO WHILE LOOP
+// IN IT CONDITION IS MET ATLEAST ONCE
+
+
+// let count = 1;
+// do {
+//     console.log(count);
+//     count++;
+// }
+// while (count<5);
+
